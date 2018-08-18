3cdf3a680cc70f32acde3f54dde6331392144eb9
Nazarii Bardiuk <nazarii@bardiuk.com>
2018-08-18 10:26:31 +0100

    change deployment commit message

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ git config user.name  'CircleCI'
 git config user.email 'job@circleci.com'
 
 git add .
-git commit -m "manual deploy"
+git commit -m "deployment for commit $CIRCLE_SHA1"
 
 git remote add origin 'https://github.com/nbardiuk/nbardiuk.github.io.git'
 git push --force origin master
