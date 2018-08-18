1c46d67f907bd19d3cd17e7a6a24f09adfad9b11
Nazarii Bardiuk <nazarii@bardiuk.com>
2018-08-18 23:27:33 +0100

    rename build stages

--- publish.sh
+++ publish.sh
@@ -7,7 +7,7 @@ git config user.name  'CircleCI'
 git config user.email 'job@circleci.com'
 
 git add .
-git commit -m "deployment for commit $CIRCLE_SHA1"
+git commit -m "deployment for $CIRCLE_SHA1 [ci skip]"
 
 git remote add origin 'https://github.com/nbardiuk/nbardiuk.github.io.git'
 git push --force origin master
